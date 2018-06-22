ec9d4fed2063336303cd54db93e0112eecbaafd7
GZS11641 <guanshanxu@corp.netease.com>
2018-06-22 16:48:31 +0800

    video

--- app/src/main/java/scut/luluteam/gutils/utils/DisplayUtil.java
+++ app/src/main/java/scut/luluteam/gutils/utils/DisplayUtil.java
@@ -3,6 +3,7 @@ package scut.luluteam.gutils.utils;
 import android.content.Context;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.WindowManager;
 
 import java.lang.reflect.Field;
@@ -13,32 +14,60 @@ import java.lang.reflect.Field;
 
 public class DisplayUtil {
 
-    private static int SCREENWIDTH;
-    private static int SCREENHEIGHT;
-    private static int DensityDpi;
-    private static int StatusBarHeight;
+    private static int SCREENWIDTH = 0;
+    private static int SCREENHEIGHT = 0;
+    private static int DensityDpi = 0;
+    private static int StatusBarHeight = 0;
 
     //但例模式：饿汉式
     private static DisplayUtil displayUtil = new DisplayUtil();
 
-    private Context mContext;
-
-
     private static String TAG = "DisplayUtil";
 
+    @Deprecated
     public static int getScreenWidth() {
         return SCREENWIDTH;
     }
 
+    @Deprecated
     public static int getScreenHeight() {
         return SCREENHEIGHT;
     }
 
-    public static int getDensityDpi() {
+    @Deprecated
+    public static int getStatusBarHeight() {
+        return StatusBarHeight;
+    }
+
+    //=======================
+    //新的API
+    //==================
+
+    public static int getScreenWidth(Context mContext) {
+        if (SCREENWIDTH == 0) {
+            displayUtil.saveScreenInfo(mContext);
+        }
+        return SCREENWIDTH;
+    }
+
+    public static int getScreenHeight(Context mContext) {
+        if (SCREENHEIGHT == 0) {
+            displayUtil.saveScreenInfo(mContext);
+        }
+        return SCREENHEIGHT;
+    }
+
+    public static int getDensityDpi(Context mContext) {
+        if (DensityDpi == 0) {
+            displayUtil.saveScreenInfo(mContext);
+        }
         return DensityDpi;
     }
 
-    public static int getStatusBarHeight() {
+    public static int getStatusBarHeight(Context mContext) {
+        if (StatusBarHeight == 0) {
+            displayUtil.saveStatusBarHeight(mContext);
+        }
         return StatusBarHeight;
     }
 
@@ -58,20 +87,19 @@ public class DisplayUtil {
      *
      * @param mContext
      */
-    public void saveScreenInfo(Context mContext) {
+    private void saveScreenInfo(Context mContext) {
 
         if (SCREENWIDTH != 0 && SCREENHEIGHT != 0) {
             return;
         }
-
         //通过Application获取屏幕信息
         DisplayMetrics metric = new DisplayMetrics();
         WindowManager mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
         mWindowManager.getDefaultDisplay().getMetrics(metric);
-        //通过Activity获取屏幕信息
-        //((Activity) mContext).getWindowManager().getDefaultDisplay().getMetrics(metric);
-        SCREENWIDTH = metric.widthPixels;     // 屏幕宽度（像素）
-        SCREENHEIGHT = metric.heightPixels;   // 屏幕高度（像素）
+        // 屏幕宽度（像素）
+        SCREENWIDTH = metric.widthPixels;
+        // 屏幕高度（像素）
+        SCREENHEIGHT = metric.heightPixels;
         DensityDpi = metric.densityDpi;
 
         Log.d(TAG, "SCREENWIDTH:\t" + SCREENWIDTH +
@@ -98,4 +126,41 @@ public class DisplayUtil {
         }
         return StatusBarHeight;
     }
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     *
+     */
+    public static int dpToPx(Context context, float dpValue) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                dpValue, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * sp 转 px
+     *
+     */
+    public static int spToPx(Context context, float dpValue) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
+                dpValue, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     *
+     */
+    public static float pxToDp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * px 转 sp
+     *
+     */
+    public static float pxToSp(Context context, float pxValue) {
+        return (pxValue / context.getResources().getDisplayMetrics().scaledDensity);
+    }
+
+
 }
